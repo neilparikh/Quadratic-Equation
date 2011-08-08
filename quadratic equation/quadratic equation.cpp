04d4eb78797cee8daf2c7d447c9372649a379318
Neil Parikh <neilp1@facebook.com>
2011-08-08 17:58:48 -0600

    Created a class to handle this instead of just variables and if statements.

--- quadratic equation/quadratic equation.cpp
+++ quadratic equation/quadratic equation.cpp
@@ -1,49 +1,75 @@
 // quadratic equation.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#ifdef _WIN32
+#include "stdafx.h" 
+#endif
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
+
+class quadratic {
+private:
+	double a;
+	double b;
+	double c;
+	double delta;
+	double x1;
+	double x2;
+public:
+	double get_x1(void) {return x1;};
+	double get_x2(void) {return x2;};
+	short number_of_roots;
+	quadratic(double user_a, double user_b, double user_c) {
+		a = user_a;
+		b = user_b;
+		c = user_c;
+		delta = b*b-4*a*c;
+		if (delta < 0) {
+		number_of_roots = 0;
+		}
+
+		if (delta == 0) {
+			number_of_roots = 1;
+			x1 = (-b + sqrt(delta))/2*a;
+		}
+
+		if (delta > 0) {
+			number_of_roots = 2;
+			x1 = (-b + sqrt(delta))/2*a;
+			x2 = (-b - sqrt(delta))/2*a;
+		}
+	}
+	void display(void) {
+		switch (number_of_roots) {
+		case 0:
+			cout<< "There are no real roots.";
+			break;
+		case 1:
+			cout<< "There is one root, which is " << x1;
+			break;
+		case 2:
+			cout<< "There are two real roots, which are " << x1 << " and " << x2;
+		}
+	}
+};
 int main() 
 {
+
 double a;
 double b;
 double c;
-double delta;
-double x1;
-double x2;
 
-cout<< "Enter the value of a:" << endl;
+cout<< "Enter the value of a: " << endl;
 cin>> a;
-cout<< "Enter the value of b:" << endl;
+cout<< "Enter the value of b: " << endl;
 cin>> b;
-cout<< "Enter the value of c:" << endl;
+cout<< "Enter the value of c: " << endl;
 cin>> c;
 
-delta = b*b-4*a*c;
-
-	if (delta < 0) {
-		cout<< "The equation has no real roots.";
-	}
-
-	x1 = (-b + sqrt(delta))/2*a;
-	x2 = (-b - sqrt(delta))/2*a;
-
-	if (delta == 0) {
-		cout<< "The equation has one root which is ";
-		cout << x1;
-	}
-
-	if (delta > 0) {
-		cout<< "The equation has two roots which are "; 
-		cout<< x1; 
-		cout<< " and ";
-		cout<< x2;
-	}
-
+quadratic instance(a, b, c);
+instance.display();
 cin.ignore();
 cin.get();
-
 }
